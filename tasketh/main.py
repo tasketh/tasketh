943d36f1a2ee62ce3ac54e74f2dbfbf103cf6259
Samhitha Bharthulwar <samhithabharthulwar@gmail.com>
2022-02-15 23:37:59 +0530

    Improved organization and readability

--- tasketh/main.py
+++ tasketh/main.py
@@ -1,16 +1,8 @@
 import discord
 import os
+from custom import *
+from tasks import *
 
-#CUSTOM VARIABLES
-task_channel = 942690426920529930
-react_emoji = '✅'
-task_command = '!@task '
-syntax_delimiter = '|'
-task_mention = '@123here'
-report_channel = 942730050531369069
-buffer_ppl = 0
-logo = "https://cdn.discordapp.com/attachments/876497372228759562/941687088624054343/29996451.png"
-#END OF CUSTOM VARIABLES
 
 client = discord.Client()
 
@@ -19,58 +11,24 @@ async def on_ready():
   print('Logged in as {0.user}'.format(client))
 
 @client.event
-async def on_message(message):
-  if message.author == client.user:
+async def on_message(message): 
+  """Function called when a message is sent"""
+  if message.author == client.user:           #checks if the message is sent by the bot
     return
 
-  if message.content.startswith(task_command):
-    commandContent = message.content[len(task_command)::]
-    commandList = commandContent.split(syntax_delimiter)
-    numPplOg = int(commandList[0])
-    numHrs = commandList[1]
-    taskName = syntax_delimiter.join(commandList[2::])
-    taskDesc = f"Number of people required: **{numPplOg}**\nNumber of hours: **{numHrs}**"
-    taskFooter = "Please keep in mind that tasks are assigned on a first come, first served basis."
-    taskEmbed = discord.Embed(title=f"New Task! {taskName}",description= taskDesc)
-    taskEmbed.set_thumbnail(url=logo)
-    taskEmbed.set_footer(text=taskFooter)
+  #task command
+  if message.content.startswith(PREFIX+"task "): #task command  
+    #Separates the command and assigns them to the dictionary.
+    commandList = message.content[len(PREFIX)+4::].strip().split(SYNTAX_DELIMITER)
+    taskDetails = {'taskUsers':int(commandList[0]), 'taskHours':commandList[1], 'taskName':SYNTAX_DELIMITER.join(commandList[2::])}
+  
+    sentTaskMsg = await sendTaskEmbed(message, client, taskDetails)
 
-    sentMsg = await client.get_channel(task_channel).send(content=task_mention, embed=taskEmbed)
+    userList = await collectResponses(sentTaskMsg, client, taskDetails)
 
-    await sentMsg.add_reaction(react_emoji)
+    await closeTask(sentTaskMsg, taskDetails)
 
-    userList = []
-    numPpl = numPplOg + buffer_ppl
-    
-    def check(reaction, user):
-      return str(reaction.emoji) == react_emoji and user!=client.user
-
-    while len(userList)<numPpl:
-      reaction, user = await client.wait_for('reaction_add', check=check)
-      if reaction.message!=sentMsg:
-        continue
-      userId = user.id
-      userList.append(userId)
-
-    await sentMsg.clear_reactions()
-    closedTaskName = "Task Closed"
-    closedTaskDesc = taskName + f"\nNumber of people required: {numPplOg}\nNumber of hours: {numHrs}"
-    closedTaskEmbed = discord.Embed(title=closedTaskName,description= closedTaskDesc)
-    taskEmbed.set_thumbnail(url =logo)
-    taskEmbed.set_footer(text=taskFooter)
-    await sentMsg.edit(embed=closedTaskEmbed)
-
-    reportDesc =  ""
-    reportFooter = "Responses are sorted according to time."
-
-    for i in range(len(userList)):
-      reportDesc+=(f'{str(i+1)}. <@{userList[i]}>\n')
-
-    report = discord.Embed(title=f"Report for {taskName}", description=reportDesc)
-    report.set_footer(text=reportFooter, icon_url =logo)
-    await client.get_channel(report_channel).send(embed=report)
-
+    await sendReport(client, taskDetails, userList)
 
 
 client.run(os.getenv('TOKEN'))
-
