486e61aa84a347686ea84bad1ed618040de98368
Rohith <rohithgeorgeninan@gmail.com>
2022-02-18 20:24:25 +0530

    Changed icon link

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ class Server:
         self.prefix = "!"
         self.syntaxDelimiter = '|'
         self.bufferUsers = 0
-        self.logo = "https://github.com/GHrohith/tasketh/blob/main/Assets/icon.png"
+        self.logo = "https://cdn.discordapp.com/attachments/942689663150342184/944189528338464778/squid.png"
         self.taskschannel = None #chennel ID, not name
         self.reportschannel = None #chennel ID, not name
         self.taskMention = 'everyone'
